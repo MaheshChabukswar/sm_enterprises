0eebf5453d0b3b2447b32313e92e71335e2031ef
MaheshChabukswar <chabukswar09@gmail.com>
2025-02-17 22:21:22 +0530

    Intro Design

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
-document.querySelector("#app").innerHTML = `
-  <div>
-    
-    <h1>Hello SM Enterprises</h1>
-   
-    
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
+import './style.css';
